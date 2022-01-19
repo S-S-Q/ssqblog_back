c856b6974f28c6a5f4fe9f74e3a20d397bcedb72
shuaishuaiqin <623923875@qq.com>
2022-01-19 13:23:03 +0800

    Jwt+SpringSecurity+Redis 实现跨域以及实现全局异常处理 比较好用的demo

--- src/main/java/com/ssq/pojo/UserLoginRequest.java
+++ src/main/java/com/ssq/pojo/UserLoginRequest.java
@@ -4,10 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserLoginRequest {
+    @NotBlank(message = "用户名不能为空")
     String username;
+    @NotBlank(message = "密码不能为空")
     String password;
 }
